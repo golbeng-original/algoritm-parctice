2f1fffc44b3dd584708768524c02f10a4075158a
June <golbeng0@gmail.com>
2025-04-10 02:10:31 +0900

    $ string chapter_6 문제 풀이
    - string chapyer_6 문제풀이

--- string/chapter_6.py
+++ string/chapter_6.py
@@ -5,12 +5,47 @@
 from typing import Dict, List, Tuple
 from collections import defaultdict 
 
-def my_solution(input:List[str]):
+def book_solution(input:str):
+    # 중앙부터 퍼저 나가는??
 
-    pass
+    def expand(left:int, right:int):
+        while left >= 0 and right < len(input) and input[left] == input[right]:
+            left -= 1
+            right += 1
+        
+        return input[left + 1:right]
+    
+    if len(input) < 2 or input == input[::-1]:
+        return input
+
+    result = ''
+    for i in range(len(input) - 1):
+        result = max(
+            result,
+            expand(i, i + 1), # 짝수개 체크
+            expand(i, i + 2), # 홀수개 체크
+            key=len
+        )
+
+    return result
+
+
+def my_solution(input:str):
+
+    ch_list = list(input)
+
+    longest_string = ''
+    for i in range(len(ch_list)):
+        for j in range(i, len(ch_list)):
+
+            substring = ''.join(ch_list[i:j+1])
+            if substring == substring[::-1] and len(substring) > len(longest_string):
+                longest_string = substring
+
+    return longest_string
 
 if __name__ == '__main__':
-    input = [
+    inputs = [
         "babad", # 정답 : bab 또는 aba
         "cbbd", # 정답 : bb
         "racecar", # 정답 : racecar
@@ -24,5 +59,9 @@ if __name__ == '__main__':
         "Aa", # 정답 : A 또는 a
         "abcbahelloollehxyz", # 정답 : abcba 또는 helloolleh
     ]
-    result = my_solution(input)
-    print(result)
+
+    for input in inputs:
+        #result = my_solution(input)
+        result = book_solution(input)
+        print(result)
+        #break
