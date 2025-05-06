ac78e1744b6522a5d00dc0b73019c70374226f5d
June <golbeng0@gmail.com>
2025-05-07 01:39:48 +0900

    $ 경로 탐색 문제풀이 추가
    - 경로 탐색 문제 풀이 추가

---
+++ csharp/pathExplore/chapter40/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+
+namespace Chapter40;
+
+using NextVertexInfos = List<(int vertex, int weight)>;
+
+// 네트워크 딜레이 타임
+public class Program {
+
+    public static int MySolution(int[][] pathInfos, int k,int n) {
+        
+        var graph = new Dictionary<int, NextVertexInfos>();
+
+        foreach(var pathInfo in pathInfos) {
+            if( graph.ContainsKey(pathInfo[0]) == false) {
+                graph.Add(pathInfo[0], new NextVertexInfos());
+            }
+
+            graph[pathInfo[0]].Add((pathInfo[1], pathInfo[2]));
+        }
+        
+        // time, 최근 노드
+        var dist = new Dictionary<int, int>();
+
+        var priorityQueue = new PriorityQueue<int, int>();
+        priorityQueue.Enqueue(k, 0);
+
+        while( priorityQueue.Count > 0 ) {
+
+            priorityQueue.TryDequeue(out var node, out var time);
+
+            if( dist.ContainsKey(node) == true) {
+                continue;
+            }
+
+            dist[node] = time;
+            if( graph.ContainsKey(node) == false ) {
+                continue;
+            }
+
+            foreach(var nextVertex in graph[node]) {
+                var accTime = time + nextVertex.weight;
+                priorityQueue.Enqueue(nextVertex.vertex, accTime);
+            }
+        }
+
+        if( dist.Count != n ) {
+            return -1;
+        }
+
+        return dist.Values.Max();
+    }
+
+    public static void Main(string[] args) {    
+        var questions = new List<(int k, int n, int[][] pathInfos)>() {
+            (
+                k: 2,
+                n: 4,
+                pathInfos: new int[][] {
+                    [2, 1, 1],
+                    [2, 3, 1],
+                    [3, 4, 1],
+                }
+            ), // 2
+            (
+                k: 1,
+                n: 5,
+                pathInfos: new int[][] {
+                    [1, 2, 1],
+                    [2, 3, 2],
+                    [1, 3, 4],
+                    [1, 4, 5],
+                    [4, 5, 6],
+                }
+            ), // 11
+        };
+    
+        foreach(var question in questions) {
+            var result = MySolution(question.pathInfos, question.k, question.n);
+            Console.WriteLine(result);
+            break;
+        }
+    }
+}
---
+++ csharp/pathExplore/chapter41/Program.cs
@@ -0,0 +1,155 @@
+﻿
+// K 경유지 내 가장 저렴한 항공권
+public class Program {
+
+    public struct Question(int n, List<int[]> edges, int src, int dest, int k)
+    {
+        public int n = n;
+        public List<int[]> edges = edges;
+        public int src = src;
+        public int dest = dest;
+        public int k = k;
+    }
+
+    public record VertexInfo(int vertex, int price);
+
+    public static int BookSolution(Question question) {
+        var graph = new Dictionary<int, List<VertexInfo>>();
+        foreach(var edge in question.edges) {
+            if( graph.ContainsKey(edge[0]) == false ) {
+                graph[edge[0]] = [];
+            }
+
+            graph[edge[0]].Add(new VertexInfo(edge[1], edge[2]));
+        }
+
+        var queue = new PriorityQueue<int, (int restTimes, int price)>();
+        queue.Enqueue(question.src, (restTimes: question.k, price: 0));
+
+        while( queue.Count > 0 ) {
+
+            queue.TryDequeue(out var vertex, out var accValue);
+
+            if( graph.ContainsKey(vertex) == false) {
+                continue;
+            }
+
+            foreach(var nextVertex in graph[vertex]) {
+                
+                var restTimes = accValue.restTimes;
+                var price = accValue.price + nextVertex.price;
+
+                if( restTimes == 0 && nextVertex.vertex == question.dest) {
+                    return price;
+                }
+
+                queue.Enqueue(nextVertex.vertex, (restTimes - 1, price));
+            }
+            
+        }
+
+        return -1;
+    }
+
+    public static int MySolution(Question question) {
+
+        var graph = new Dictionary<int, List<VertexInfo>>();
+        foreach(var edge in question.edges) {
+            if( graph.ContainsKey(edge[0]) == false ) {
+                graph[edge[0]] = [];
+            }
+
+            graph[edge[0]].Add(new VertexInfo(edge[1], edge[2]));
+        }
+        
+        var queue = new PriorityQueue<int, int>();
+        queue.Enqueue(question.src, -1);
+
+        var cost = new Dictionary<(int vertex, int times), int>();
+        
+        while(queue.Count > 0) {
+            
+            queue.TryDequeue(out var vertex, out var times);
+
+            if( graph.ContainsKey(vertex) == false ) {
+                continue;
+            }
+
+            var prevKey = (vertex: vertex, times: times);
+            if( cost.TryGetValue(prevKey, out var prevPrice) == false) {
+                prevPrice = 0;
+            }
+
+            foreach(var nextVertex in graph[vertex]) {
+
+                var key = (vertex: nextVertex.vertex, times: prevKey.times + 1);
+
+                cost[key] = prevPrice + nextVertex.price;
+
+                if( key.vertex == question.dest && key.times == question.k) {
+                    return cost[key];
+                }
+
+                queue.Enqueue(nextVertex.vertex, key.times);
+            }
+        }
+
+        return -1;
+    }
+
+    public static void Main(string[] args) {
+        var questions = new List<Question>() {
+            new Question(
+                n: 3,
+                edges: new List<int[]> {
+                    new int[]{0, 1, 100},
+                    new int[]{1, 2, 100},
+                    new int[]{0, 2, 500},
+                },
+                src: 0,
+                dest: 2,
+                k: 0
+            ), // 500
+            new Question(
+                n: 3,
+                edges: new List<int[]> {
+                    new int[]{0, 1, 100},
+                    new int[]{1, 2, 100},
+                    new int[]{0, 2, 500},
+                },
+                src: 0,
+                dest: 2,
+                k: 1
+            ), // 200
+            new Question(
+                n: 3,
+                edges: new List<int[]> {
+                    new int[]{0, 1, 100},
+                    new int[]{1, 2, 100},
+                    new int[]{0, 2, 500},
+                },
+                src: 0,
+                dest: 2,
+                k: 2
+            ), // -1
+            new Question(
+                n: 4,
+                edges: new List<int[]> {
+                    new int[]{0, 1, 100},
+                    new int[]{1, 2, 100},
+                    new int[]{0, 2, 500},
+                    new int[]{2, 3, 100},
+                },
+                src: 0,
+                dest: 3,
+                k: 2
+            ), // 600, //300
+        };
+
+        foreach(var question in questions) {
+            //var result = MySolution(question);
+            var result = BookSolution(question);
+            Console.WriteLine(result);
+        }
+    }
+}
