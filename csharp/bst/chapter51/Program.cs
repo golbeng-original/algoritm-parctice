889399ee4e8992a9491e1eda01242576f461187d
June <golbeng0@gmail.com>
2025-05-08 15:00:52 +0900

    $ chapter52 추가
    - chapter53 추가

---
+++ csharp/bst/chapter51/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+using utils;
+
+public class Program {
+
+    public static void MySolution(int?[] serializedTree) {
+        var root = TreeNode.createFromRaw(serializedTree);
+
+        var acc = 0;
+        void Dfs(TreeNode? node) {
+            if( node == null ) {
+                return;
+            }
+
+            Dfs(node.Right);
+            acc += node.Value;
+            node.Value = acc;
+            Dfs(node.Left);
+
+        }
+
+        Dfs(root);
+        var serialized = root.Serialize();
+        Console.WriteLine($"[{string.Join(", ", serialized)}]");
+    }
+
+    public static void Main(string[] args) {
+        var questions = new int?[][] {
+            [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+        };
+
+        foreach(var question in questions) {
+            MySolution(question);
+        }
+    }
+}
---
+++ csharp/bst/chapter52/Program.cs
@@ -0,0 +1,47 @@
+﻿
+// 이진 탐색 트리 합의 범위
+using utils;
+
+public class Program {
+
+    public record Question(int?[] serializedTree, int L, int R);
+
+    public static void MySolution(Question question) {
+        var root = TreeNode.createFromRaw(question.serializedTree);
+
+        var total = 0;
+        int Dfs(TreeNode? node) {
+            if( node == null ) {
+                return 0;
+            }
+
+            if( node.Value < question.L) {
+                return Dfs(node.Right);
+            }
+
+            if( node.Value > question.R) {
+                return Dfs(node.Left);
+            }
+
+            return node.Value + Dfs(node.Left) + Dfs(node.Right);
+        }
+
+        total = Dfs(root);
+        Console.WriteLine(total);
+    }
+
+    public static void Main(string[] args) {
+
+        var questions = new Question[] {
+            new Question(
+                serializedTree: [10, 5, 15, 3, 7, null, 18],
+                L: 7,
+                R: 15
+            )
+        };
+
+        foreach(var question in questions) {
+            MySolution(question);
+        }
+    }
+}
